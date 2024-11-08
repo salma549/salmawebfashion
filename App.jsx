fb8d88ab74b0394e9f1c914b78bc7e423874c037
Salma Sanubar <117900157+salma549@users.noreply.github.com>
2024-11-08 10:05:26 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import { Men } from './components/Men';
+import {Women} from './components/Women';
+import ProductDetail from './components/ProductDetails';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
